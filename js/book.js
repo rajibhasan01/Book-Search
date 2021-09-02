947d1d6cc9305173576a5b9e73d302e321b31370
unknown <hasan.rajib1996@gmail.com>
2021-09-02 13:36:06 +0600

    update

--- js/book.js
+++ js/book.js
@@ -6,8 +6,11 @@ const searchBook = () => {
     const totalFound = document.getElementById('found-number');
         totalFound.innerText = '';
 
-    // spinner function call
+    // spinner toggle on
     displaySpinner('block');
+
+    // error msg toggle off
+    displayToast('none');
 
     if(searchText == ''){
         displayMassage('Invalid input, please give a valid book name!');
@@ -26,11 +29,16 @@ const searchBook = () => {
 
 // display spinner
 const displaySpinner = (value) =>{
-    const spinnerField = document.getElementById('spinner-field').style.display = value;
+    document.getElementById('spinner-field').style.display = value;
+}
+// display toast
+const displayToast = (value) =>{
+    document.getElementById('toast-msg-box').style.display = value;
 }
 
 // display error massage arrow function
 const displayMassage = errorMsg =>{
+        displayToast('block');
         const container = document.getElementById('result-container');
         const totalFound = document.getElementById('found-number');
         totalFound.innerText = '';
@@ -69,14 +77,14 @@ const displayBooks = books =>{
        div.classList.add('col');
        div.innerHTML = `
             <div onclick = "loadBook('${book.author_key}')" class="card h-100 overflow-hidden" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                <div class="overflow-hidden ">
+                <div class="overflow-hidden w-50 mx-auto my-4">
                     <img src = ${source} class="card-img-top image" alt="...">
                 </div>
-                <div class="card-body">
-                    <h5 class="card-title">${book.title ? book.title : 'Book name not available'}</h5>
-                    <p class="card-text text-muted">Author: ${book.author_name ? book.author_name[0] : 'Author info not available'} <br>
-                    Publisher: ${book.publisher[0] ? book.publisher[0] : 'Publisher info not available'} <br>
-                    ${book.first_publish_year ? book.first_publish_year : 'Publise year not available'}</p>
+                <div class="card-body text-center">
+                    <h5 class="card-title">${book.title ? book.title : 'N/A'}</h5>
+                    <p class="card-text"><span class="text-danger fst-italic">${book.author_name ? book.author_name[0] : 'N/A'} </span> <br>
+                    Publisher : ${book.publisher[0] ? book.publisher[0] : 'N/A'} <br>
+                    1st Published : <span class="fw-bold text-secondary">${book.first_publish_year ? book.first_publish_year : 'N/A'}</span></p>
                 </div>
             </div>
        `;
@@ -95,23 +103,31 @@ const loadBook = id =>{
     .catch(() => displayMassage("Sorry! Something went wrong. Try again later."));
 }
 
-const displaySingleMeal = item => {
+const displaySingleMeal = book => {
     const containerForMeal = document.getElementById('container-single-item');
-    console.log(item);
     // clear inner content
     containerForMeal.textContent = '';
+
+    // set image url
+    source =`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+
+    // undifinied picture handle
+    if(!book.cover_i){
+        source = 'image/image-not-available.jpg';
+    }
+    
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-            <img src="https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg" class="card-img-top image1" alt="...">
+            <img src=${source} class="card-img-top image1" alt="...">
             <button class="btn-close me-2 m-auto cross-btn p-2" data-bs-dismiss="modal"></button>
             <div class="card-body">
-                <h5 class="card-title">${item.title}</h5>
-                <p class="card-text text-muted">${item.author_name[0]}</p>
-                <p class="card-text text-muted">${item.publisher[0]}</p>
-                <p class="card-text text-muted">${item.publish_date}</p>
+                <h5 class="card-title">${book.title ? book.title : 'Book name not available'}</h5>
+                <p class="card-text text-muted">${book.author_name ? book.author_name[0] : 'Author info not available'}</p>
+                <p class="card-text text-muted">${book.publisher ? book.publisher[0] : 'Publisher info not available'} </p>
+                <p class="card-text text-muted">${book.first_publish_year ? book.first_publish_year : 'Publise year not available'}</p>
                 <div class = "d-md-flex justify-content-md-end">
-                    <a href="##" target="_blank" class="btn btn-warning">Read</a>
+                    <a href="##" target="_blank" class="btn btn-warning">Read Details</a>
                 </div>
             </div>
     `;
